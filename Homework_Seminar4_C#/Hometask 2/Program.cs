dc5bc9e92b37d9c1eb2c793ac061df1f9cb1994b
Andrei Savinkov <andrewsavinkov@gmail.com>
2022-09-27 15:51:03 +0500

    написал код

---
+++ Homework_Seminar4_C#/Hometask 2/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+int SumOfDigits(string line)
+{
+    Console.WriteLine(line);
+    string number = Console.ReadLine()??"";
+    int sum = 0;
+    for (int i = 0; i < number.Count(); i++)
+    {
+        int charToInt = number[i]-'0';
+        sum = sum + charToInt;
+    }
+    return sum;
+}
+
+int number = SumOfDigits("Введите число: ");
+Console.WriteLine($"Сумма цифр числа равна: {number}");
